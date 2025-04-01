e697862c221ce84d1b735d988608af478066cfe4
550tealeaves <nastasia.claire89@gmail.com>
2025-04-01 18:18:14 -0400

    moved test files and deleted unused files

--- test.js
+++ test.js
@@ -4,17 +4,17 @@ var x, y, tox, toy;
 //var zoom = .005; //zoom step per mouse tick 
 
 function preload() {
-    baseball = loadImage("images/baseball.png")
-    player = loadImage("images/player.png");
-    jackie = loadImage("images/jackie.PNG");
-    hank = loadImage("images/hank-aaron.png");
-    satchel = loadImage("images/satchel-paige.png");
-    larry = loadImage("images/larry-doby.png");
-    moses = loadImage("images/moses-fleetwood-walker.png");
-    frank = loadImage("images/frank-robinson.png");
-    mookie = loadImage("images/mookie-betts.png");
-    jeter = loadImage("images/derek-jeter.png");
-    prince = loadImage("images/prince-fielder.png");
+    baseball = loadImage("images/other/baseball.png")
+    player = loadImage("images/other/player.png");
+    jackie = loadImage("images/other/jackie.PNG");
+    hank = loadImage("images/other/hank-aaron.png");
+    satchel = loadImage("images/other/satchel-paige.png");
+    larry = loadImage("images/other/larry-doby.png");
+    moses = loadImage("images/other/moses-fleetwood-walker.png");
+    frank = loadImage("images/other/frank-robinson.png");
+    mookie = loadImage("images/other/mookie-betts.png");
+    jeter = loadImage("images/other/derek-jeter.png");
+    prince = loadImage("images/other/prince-fielder.png");
 }
 
 //code from here: https://gist.github.com/companje/5478fff07a18a1f4806df4cf77ae1048
